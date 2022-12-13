6188cb13f19c7fd23b928fb6afbb2a1ffc72750a
ksummers112 <43285227+ksummers112@users.noreply.github.com>
2022-12-13 16:01:34 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# POC-ProdMan-NewNav
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-bfp5xc)
---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialExampleModule } from '../material.module';
+import { CardMediaSizeExample } from './card-media-size-example';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import { HttpClientModule } from '@angular/common/http';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+  declarations: [CardMediaSizeExample],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    MaterialExampleModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
+  providers: [],
+  bootstrap: [CardMediaSizeExample],
+})
+export class AppModule {}
