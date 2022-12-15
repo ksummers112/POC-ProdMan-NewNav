f7d69940f01ab29def535482082a41f978a019d5
ksummers112 <43285227+ksummers112@users.noreply.github.com>
2022-12-15 07:50:29 -0600

    Adding settings menu

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,19 +1,8 @@
 import { Component } from '@angular/core';
 
-/**
- * @title Card with media size
- */
 @Component({
   selector: 'app-card',
   templateUrl: 'card.component.html',
   styleUrls: ['card.component.css'],
 })
-export class CardComponent {
-  longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
-  from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
-  originally bred for hunting.`;
-}
-
-/**  Copyright 2022 Google LLC. All Rights Reserved.
-    Use of this source code is governed by an MIT-style license that
-    can be found in the LICENSE file at https://angular.io/license */
+export class CardComponent {}
